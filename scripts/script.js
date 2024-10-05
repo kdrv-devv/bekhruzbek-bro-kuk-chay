ebe2112caefefcee994bd16581abd9c735c9282d
kdrv dev <ffeodalik@gmail.com>
2024-10-06 00:16:17 +0500

    kun-tun bosilganda y harfi o'zgaradi

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,33 @@
-
 let btn_primary = document.querySelector(".btn-primary");
 let choynak = document.querySelector(".choynak");
-let yletter = document.querySelector(".water-fill-y");
 let rect = document.querySelector(".water");
+let raxmat = document.querySelector(".eraxmat");
+let newDay = document.querySelector(".theme-switch__checkbox");
+let textElement = document.querySelector(".stroke-text");
 
 rect.setAttribute("class", "");
 
-btn_primary.addEventListener("mouseover", function() {
-    choynak.classList.add("choynak-anim");
-
-    rect.setAttribute("class", "water"); 
-
-    yletter.classList.add("water-fill-y");
+btn_primary.addEventListener("mouseover", function () {
+  choynak.classList.add("choynak-anim");
+  rect.setAttribute("class", "water");
 });
 
-
-
-
-btn_primary.addEventListener("mouseout", function() {
-    choynak.classList.remove("choynak-anim");
-
-    rect.setAttribute("class", "");
-
-    yletter.classList.remove("water-fill-y");
+btn_primary.addEventListener("mouseout", function () {
+  choynak.classList.remove("choynak-anim");
+  rect.setAttribute("class", "");
 });
 
-
-// kun-tun 
-
-// let rejimBtn = document.querySelector(".night_button_box")
-
-// let body = document.body
-
-
-
-// rejimBtn.addEventListener("click",function(){
-
-//     body.classList.toggle("qora")
-
-
-// })
-
-
-// raxmat
-
-let raxmat = document.querySelector(".eraxmat");
-
-btn_primary.addEventListener("click", function() {
-    raxmat.style.left = "0"; 
+btn_primary.addEventListener("click", function () {
+  raxmat.style.left = "0";
 });
 
+// kun tun rejimi
+newDay.addEventListener("change", function () {
+  document.body.classList.toggle("qora");
 
-// kun tun 2 
-
-let newDay = document.querySelector(".theme-switch__checkbox")
-
-
-newDay.addEventListener("change",function(){
-
-    document.body.classList.toggle("qora")
-
-})
+  if (newDay.checked) {
+    textElement.setAttribute("stroke", "rgba(255, 255, 255, 0.5)");
+  } else {
+    textElement.setAttribute("stroke", "rgba(0, 0, 0, 0.5)");
+  }
+});
