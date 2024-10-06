c09f9c2e26508b181960380cb90c8167804c8d87
kdrv dev <ffeodalik@gmail.com>
2024-10-06 13:52:43 +0500

    bg togrilandi

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,11 @@
 let btn_primary = document.querySelector(".btn-primary");
 let choynak = document.querySelector(".choynak");
 let rect = document.querySelector(".water");
-let raxmat = document.querySelector(".eraxmat");
 let newDay = document.querySelector(".theme-switch__checkbox");
 let textElement = document.querySelector(".stroke-text");
-
+let textshadow = document.querySelector(".h1bu")
 rect.setAttribute("class", "");
-
+let subtitle = document.querySelector(".subtitle")
 btn_primary.addEventListener("mouseover", function () {
   choynak.classList.add("choynak-anim");
   rect.setAttribute("class", "water");
@@ -24,6 +23,10 @@ btn_primary.addEventListener("click", function () {
 // kun tun rejimi
 newDay.addEventListener("change", function () {
   document.body.classList.toggle("qora");
+  document.body.classList.toggle("dark-mode");
+  textshadow.classList.toggle("text-shadow")
+  subtitle.classList.toggle("dark-subtitle")
+
 
   if (newDay.checked) {
     textElement.setAttribute("stroke", "rgba(255, 255, 255, 0.5)");
