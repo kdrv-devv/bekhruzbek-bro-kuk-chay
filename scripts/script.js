182418dfcdd6653bf300523bf62425c849fe83a7
kdrv dev <ffeodalik@gmail.com>
2024-10-07 11:32:09 +0500

    sound qoshildi

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,24 @@ let choynak = document.querySelector(".choynak");
 let rect = document.querySelector(".water");
 let newDay = document.querySelector(".theme-switch__checkbox");
 let textElement = document.querySelector(".stroke-text");
-let textshadow = document.querySelector(".h1bu")
+let textshadow = document.querySelector(".h1bu");
+let avatarshadow = document.querySelector(".dark-avatar");
+let subtitle = document.querySelector(".subtitle");
+let teaSound = document.querySelector(".tea-sound")
+
+
+// Boshlang'ich suvdan foydalanish uchun
 rect.setAttribute("class", "");
-let subtitle = document.querySelector(".subtitle")
+
+// Hover uchun animatsiyalar
 btn_primary.addEventListener("mouseover", function () {
   choynak.classList.add("choynak-anim");
   rect.setAttribute("class", "water");
+  setTimeout(function() {
+    teaSound.play()
+}, 1000);
+
+
 });
 
 btn_primary.addEventListener("mouseout", function () {
@@ -16,21 +28,28 @@ btn_primary.addEventListener("mouseout", function () {
   rect.setAttribute("class", "");
 });
 
-btn_primary.addEventListener("click", function () {
-  raxmat.style.left = "0";
-});
-
-// kun tun rejimi
+// Kun-tun rejimi o'zgarishi
 newDay.addEventListener("change", function () {
   document.body.classList.toggle("qora");
   document.body.classList.toggle("dark-mode");
-  textshadow.classList.toggle("text-shadow")
-  subtitle.classList.toggle("dark-subtitle")
+  textshadow.classList.toggle("text-shadow");
+  subtitle.classList.toggle("dark-subtitle");
+  btn_primary.classList.toggle("dark-btn");
+  avatarshadow.classList.toggle("dark-avatar");
 
-
+  // Stroke rangini o'zgartirish
   if (newDay.checked) {
-    textElement.setAttribute("stroke", "rgba(255, 255, 255, 0.5)");
+    textElement.style.stroke = "rgba(255, 255, 255, 0.5)"; // oq rang
   } else {
-    textElement.setAttribute("stroke", "rgba(0, 0, 0, 0.5)");
+    textElement.style.stroke = "rgba(0, 0, 0, 0.5)"; // qora rang
   }
+});
+
+
+// boshqa pagega o'tkazish
+
+btn_primary.addEventListener("click", function () {
+  setTimeout(function() {
+    window.location.href = "purchase.html"; 
+}, 4000);
 });
